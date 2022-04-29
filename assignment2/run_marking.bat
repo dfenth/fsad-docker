1c60e5d8d3b0d02fb323f17eec56ffae6359d3f0
Matty <matty258987@gmail.com>
2022-04-29 14:19:10 +0100

    Fix docker build command for Windows

--- assignment2/run_marking.bat
+++ assignment2/run_marking.bat
@@ -1,7 +1,7 @@
 @echo off
 start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
-docker build -f "pgdocker\Dockerfile" -t pgdocker - < pgdocker.tar.gz
+docker build -f "pgdocker/Dockerfile" -t pgdocker - < pgdocker.tar.gz
 docker run --rm -v "%cd%/Test:/data" pgdocker
 
 echo Press any key to exit...
